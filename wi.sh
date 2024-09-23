304899f1fac9494d89b51d48f80648b37404389d
Nidoxs <nidoxsdevelopment@gmail.com>
2024-09-23 10:59:22 +0100

    Remove recursive flag from rm commands in `wi.sh` script

--- wi.sh
+++ wi.sh
@@ -3,8 +3,8 @@ PROJECT_FILE="${1:-$DEFAULT_PROJECT_FILE}"
 
 rm -rf "$(pwd)/Packages"
 rm -rf "$(pwd)/ServerPackages"
-rm -rf "$(pwd)/sourcemap.json"
-rm -rf "$(pwd)/wally.lock"
+rm -f "$(pwd)/sourcemap.json"
+rm -f "$(pwd)/wally.lock"
 wally install
 rojo sourcemap "$PROJECT_FILE" >> sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages
